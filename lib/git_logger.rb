5eca59274243955fede23949310548e6446d7cf4
Alex Rocha <alex.rochas@yahoo.com.br>
2017-06-24 21:25:00 -0300

    feat: add mongo env url

--- lib/git_logger.rb
+++ lib/git_logger.rb
@@ -5,7 +5,7 @@ require 'mongo'
 class GitLogger < Sinatra::Base
 
   post '/command' do
-    client = Mongo::Client.new([ 'localhost:27017' ], :database => 'git_logger')
+    client = Mongo::Client.new(ENV['MONGODB_URI']||='mongodb://localhost:27017/git_logger')
     collection = client[:commands]
 
     request.body.rewind  # in case someone already read it
